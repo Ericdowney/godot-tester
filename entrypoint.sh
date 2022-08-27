7d107b23b9de47cbc0f764d880ff6bc384b03eba
edowney <edowney@uncannyapps.com>
2022-08-26 22:19:55 -0400

    Updated entrypoint.sh, removing check_assert and pass rate functionality. Script now uses the GUT command line output to determine test failure or success.

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,70 +9,13 @@ PROJECT_DIRECTORY=$3
 IS_MONO=$4
 IMPORT_TIME=$5
 TEST_TIME=$6
-MINIMUM_PASSRATE=$7
-TEST_DIR=$8
-DIRECT_SCENE=$9
-# args above 9 require brackets
-ASSERT_CHECK=${10}
-MAX_FAILS=${11}
-IGNORE_ERROR=${12}
-CONFIG_FILE=${13}
+DIRECT_SCENE=$7
+IGNORE_ERROR=$8
+CONFIG_FILE=$9
 
 GODOT_SERVER_TYPE="headless"
-TESTS=0
-FAILED=0
 CUSTOM_DL_PATH="~/custom_dl_folder"
-RUN_OPTIONS="-s addons/gut/gut_cmdln.gd -gdir=${TEST_DIR} -ginclude_subdirs -gexit"
-
-# credit: https://stackoverflow.com/questions/24283097/reusing-output-from-last-command-in-bash
-# capture the output of a command so it can be retrieved with ret
-cap() { tee /tmp/capture.out; }
-
-# return the output of the most recent command that was captured by cap
-ret() { cat /tmp/capture.out; }
-
-check_by_assert() {
-    script_error_fns=()
-
-    teststring="Totals"
-    script_error="SCRIPT ERROR"
-
-    test_set=0
-
-    while read line; do
-        # credit : https://stackoverflow.com/questions/17998978/removing-colors-from-output
-        temp=$(echo $line | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g')
-        # can see with below line all the extra characters that echo ignores
-        # echo LINE: $temp
-        if [[ $temp =~ ^$script_error ]]; then
-            echo "script error found at $temp"
-            if [ ${IGNORE_ERROR} = "false" ]; then
-                FAILED=$((FAILED + 1))
-                echo failed test count increased: $FAILED
-                echo
-                echo
-            fi
-            continue
-        elif [[ $temp =~ ^$teststring ]]; then
-            test_set=1
-            continue
-        fi
-
-        if [ "$test_set" -eq "0" ]; then
-            continue
-        elif [[ $temp =~ ^[0-9]+[[:space:]]+(passed)[[:space:]]+[0-9]+[[:space:]]+(failed) ]]; then
-            passes=$(echo $temp | awk '{print $1}')
-            fails=$(echo $temp | awk '{print $3}')
-            FAILED=$((FAILED + fails))
-            TESTS=$((TESTS + FAILED + passes))
-            echo "total failed tests $FAILED"
-            echo "total tests $TESTS"
-            echo
-            echo
-            break
-        fi
-    done <<<$(ret)
-}
+RUN_OPTIONS="-s addons/gut/gut_cmdln.gd -ginclude_subdirs -gexit"
 
 if [ "$RELEASE_TYPE" = "stable" ]; then
     DL_PATH_SUFFIX=""
@@ -131,11 +74,14 @@ if [ "$IS_MONO" = "true" ]; then
     # workaround for -e -q and -e with timeout failing
     # credit: https://github.com/Kersoph/open-sequential-logic-simulation/pull/4/files
     timeout ${IMPORT_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}/${FULL_GODOT_NAME}.64 --editor addons/gut/.cli_add/__rebuilder_scene.tscn
-    timeout ${TEST_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}/${FULL_GODOT_NAME}.64 ${RUN_OPTIONS} 2>&1 | cap
+    timeout ${TEST_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}/${FULL_GODOT_NAME}.64 ${RUN_OPTIONS} 2>&1
 else
     timeout ${IMPORT_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION} --editor addons/gut/.cli_add/__rebuilder_scene.tscn
-    timeout ${TEST_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION} ${RUN_OPTIONS} 2>&1 | cap
+    timeout ${TEST_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION} ${RUN_OPTIONS} 2>&1
 fi
+
+# Check GUT output and save exit value, 0 = success, 1 = fail
+exitval=$?
 
 # removing scene used to rebuild import files
 rm -rf ./addons/gut/.cli_add/__rebuilder.gd
@@ -143,38 +89,5 @@ rm -rf ./addons/gut/.cli_add/__rebuilder_scene.tscn
 
 rm -rf ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}
 rm -f ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}.zip
-
-# parsing test output to fill test count and pass count variables
-check_by_assert
-
-passrate=".0"
-endmsg=""
-if [ "$TESTS" -eq "0" ]; then
-    exitval=1
-    endmsg="Tests failed due to timeout or there were no tests to run\n"
-else
-    passrate=$(echo "scale=3; ($TESTS-$FAILED)/$TESTS" | bc -l)
-    echo -e "\n${passrate} pass rate\n"
-    if (($(echo "$passrate >= $MINIMUM_PASSRATE" | bc -l))); then
-        exitval=0
-    else
-        endmsg="Tests failed due to low passrate\n"
-        exitval=1
-    fi
-
-    if [[ "$MAX_FAILS" != "false" ]]; then
-        if [[ "$FAILED" -gt "$MAX_FAILS" ]]; then
-            endmsg="Tests failed due to fail count of ${FAILED} exceeding maximum of ${MAX_FAILS}"
-            exitval=1
-        fi
-    fi
-fi
-
-if [ "$endmsg" != "" ]; then
-    echo -e "Note: Tests may appear to pass on SCRIPT ERROR, but they were just ignored by GUT"
-    echo -e "This action ensures those will fail"
-    echo
-    echo -e "${endmsg}"
-fi
 
 exit ${exitval}
